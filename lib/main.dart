8258630e1744b37d922060906a2579b0fafde7c5
Antonio B <bojorquez29an@gmail.com>
2023-10-01 21:30:08 -0700

    todo added main

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:habit_tracker/app/app.dart';
 
 void main() {
+  // todo refactorizar lista de habitos para que sea una clase
+  // todo guardar los habitos local storage
+  // todo agregar heatmapcalendar
+  // todo agregar temas
   runApp(const App());
 }
